cdf6c3b2ffb02976284bd46721128d0ce9d92eeb
Harsh jha <harshjha8453@gmail.com>
2026-05-29 18:48:31 +0530

    remove dotenv loading from main class

--- src/main/java/com/nivora/nivora_finance_backend/NivoraFinanceBackendApplication.java
+++ src/main/java/com/nivora/nivora_finance_backend/NivoraFinanceBackendApplication.java
@@ -1,6 +1,5 @@
 package com.nivora.nivora_finance_backend;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,8 +8,6 @@ public class NivoraFinanceBackendApplication {
 
 	public static void main(String[] args) {
 
-		Dotenv dotenv = Dotenv.configure()
-						.load();
 
 		SpringApplication.run(NivoraFinanceBackendApplication.class, args);
 	}
