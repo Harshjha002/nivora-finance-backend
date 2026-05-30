4d352e4a6257f419047ba5bddd9b0a4ad937c6e7
Harsh jha <harshjha8453@gmail.com>
2026-05-30 15:49:14 +0530

    implement redis otp verification and user validation flow

--- src/main/java/com/nivora/nivora_finance_backend/auth/service/impl/AuthServiceImpl.java
+++ src/main/java/com/nivora/nivora_finance_backend/auth/service/impl/AuthServiceImpl.java
@@ -9,105 +9,143 @@ import com.nivora.nivora_finance_backend.auth.entity.User;
 import com.nivora.nivora_finance_backend.auth.repository.UserRepository;
 import com.nivora.nivora_finance_backend.auth.service.AuthService;
 import com.nivora.nivora_finance_backend.common.exception.InvalidCredentialsException;
+import com.nivora.nivora_finance_backend.common.exception.InvalidOtpException;
 import com.nivora.nivora_finance_backend.common.exception.ResourceNotFoundException;
 import com.nivora.nivora_finance_backend.common.exception.UserAlreadyExistsException;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import java.time.Duration;
+import java.util.concurrent.ThreadLocalRandom;
+
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
 
 @Service
 @AllArgsConstructor
 @Slf4j
 public class AuthServiceImpl implements AuthService {
 
-    private final UserRepository repo;
-    private final PasswordEncoder passwordEncoder;
+        private final UserRepository repo;
+        private final PasswordEncoder passwordEncoder;
+        private final RedisTemplate<String, String> redisTemplate;
 
-    @Override
-    public void signup(SignupRequest req) {
+        @Override
+        public void signup(SignupRequest req) {
 
-        String reqEmail = req.getEmail();
-        String reqName = req.getName();
-        String reqPassword = req.getPassword();
+                String reqEmail = req.getEmail();
+                String reqName = req.getName();
+                String reqPassword = req.getPassword();
 
-        boolean emailExists = repo.existsByEmail(reqEmail);
-        if(emailExists){
-            throw new UserAlreadyExistsException(
-                    "Email already exists"
-            );
+                boolean emailExists = repo.existsByEmail(reqEmail);
+                if (emailExists) {
+                        throw new UserAlreadyExistsException(
+                                        "Email already exists");
+                }
+
+                String encodedPassword = passwordEncoder.encode(reqPassword);
+
+                User newUser = User.builder()
+                                .name(reqName)
+                                .password(encodedPassword)
+                                .email(reqEmail)
+                                .verified(false)
+                                .build();
+
+                repo.save(newUser);
+
+                String otp = generateOtp();
+
+                redisTemplate.opsForValue().set(
+                                "otp:" + reqEmail,
+                                otp,
+                                Duration.ofMinutes(5));
+
+                log.info("OTP for {} : {}", reqEmail, otp);
+
         }
 
-        String encodedPassword =
-                passwordEncoder.encode(reqPassword);
+        @Override
+        public AuthResponse verifyOtp(OtpVerifyRequest req) {
 
-        User newUser = User.builder()
-                .name(reqName)
-                .password(encodedPassword)
-                .email(reqEmail)
-                .verified(false)
-                .build();
+                String storedOtp = redisTemplate.opsForValue()
+                                .get("otp:" + req.getEmail());
 
-        repo.save(newUser);
+                if (storedOtp == null) {
+                        throw new InvalidOtpException(
+                                        "OTP expired");
+                }
 
-        generateOtp();
+                if (!storedOtp.equals(req.getOtp())) {
+                        throw new InvalidOtpException(
+                                        "Invalid OTP");
+                }
+                User user = repo.findByEmail(req.getEmail())
+                                .orElseThrow(() -> new ResourceNotFoundException(
+                                                "User not found"));
 
-    }
+                user.setVerified(true);
 
-    @Override
-    public AuthResponse verifyOtp(OtpVerifyRequest req) {
-        return null;
-    }
+                repo.save(user);
 
-   @Override
-public AuthResponse login(LoginRequest req) {
+                redisTemplate.delete(
+                                "otp:" + req.getEmail());
 
-    User user = repo.findByEmail(req.getEmail())
-            .orElseThrow(() ->
-                    new ResourceNotFoundException(
-                            "User not found"
-                    ));
+                log.info(
+                                "User verified successfully: {}",
+                                req.getEmail());
 
-    boolean passwordMatches =
-            passwordEncoder.matches(
-                    req.getPassword(),
-                    user.getPassword()
-            );
+                return AuthResponse.builder()
+                                .message("OTP verified successfully")
+                                .token(null)
+                                .build();
 
-    if (!passwordMatches) {
+        }
 
-        log.warn(
-                "Invalid login attempt for email: {}",
-                req.getEmail()
-        );
+        @Override
+        public AuthResponse login(LoginRequest req) {
 
-        throw new InvalidCredentialsException(
-                "Invalid credentials"
-        );
-    }
+                User user = repo.findByEmail(req.getEmail())
+                                .orElseThrow(() -> new ResourceNotFoundException(
+                                                "User not found"));
 
-    log.info(
-            "Login successful for email: {}",
-            req.getEmail()
-    );
+                boolean passwordMatches = passwordEncoder.matches(
+                                req.getPassword(),
+                                user.getPassword());
 
-    return AuthResponse.builder()
-            .message("Login successful")
-            .token(null)
-            .build();
-}
-    @Override
-    public void logout() {
+                if (!passwordMatches) {
 
-    }
+                        log.warn(
+                                        "Invalid login attempt for email: {}",
+                                        req.getEmail());
 
-    @Override
-    public UserProfileResponse getCurrentUser() {
-        return null;
-    }
+                        throw new InvalidCredentialsException(
+                                        "Invalid credentials");
+                }
 
-    private String generateOtp() {
-        return "otp generated";
-    }
+                log.info(
+                                "Login successful for email: {}",
+                                req.getEmail());
+
+                return AuthResponse.builder()
+                                .message("Login successful")
+                                .token(null)
+                                .build();
+        }
+
+        @Override
+        public void logout() {
+
+        }
+
+        @Override
+        public UserProfileResponse getCurrentUser() {
+                return null;
+        }
+
+        private String generateOtp() {
+                return String.valueOf(
+                                ThreadLocalRandom.current().nextInt(100000, 1000000));
+        }
 }
