9804a5260af46f99da689a110e633ab7564cc36e
Harsh jha <harshjha8453@gmail.com>
2026-07-06 13:19:19 +0530

    test: add Testcontainers integration test for transfer workflow

---
+++ src/test/java/com/nivora/nivora_finance_backend/integration/BaseIntegrationTest.java
@@ -0,0 +1,26 @@
+package com.nivora.nivora_finance_backend.integration;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+
+@SpringBootTest
+@Testcontainers
+public abstract class BaseIntegrationTest {
+
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:17");
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+
+    }
+
+}
